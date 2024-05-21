463f7f517efb3df4647fc5ce72219adacb974c80
itforget CORDEIRO <itforget11@gmail.com>
2024-05-21 19:18:58 +0000

    add: main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,54 +1,24 @@
-"use client"
-import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
-import { fetchProducts } from "./utils/getProducts";
 
 export default function Home() {
-  const { data, isLoading } = useQuery({
-    queryKey: ["products"],
-    queryFn: fetchProducts,
-    
-    })
-console.log(data)
   return (
-    <div className="h-screen w-screen">
-      <header className="flex flex-col items-center  py-2">
-      <h1 className="text-4xl font-extrabold">Produtos</h1>
-      <p className="mt-3 text-lg text-gray-500">Lista de produtos</p>
-      </header>
-      <main className="flex flex-row justify-center items-center gap-5 h-auto rounded-lg border-2 border-zinc-300 mx-16 p-4">
-        {isLoading ? <p className="font-bold text-2xl">Carregando Produtos</p> : data?.map((products, index) => {
-          return (
-            <div key={index} className="flex flex-col items-center gap-4 border-2 border-zinc-600 p-3 rounded-lg">
-              <Image
-                className="rounded-lg border-2 border-zinc-300"
-                src={products.imgUrl}
-                alt={products.name}
-                width={180}
-                height={180}
-              />
-              <h2 className="text-2xl font-bold">{products.name}</h2>
-              <p className="text-lg font-bold">{products.price.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Comprar</button>
-                <div
-                  onMouseEnter={(e) =>
-                    e.currentTarget
-                      .querySelector("p")
-                      .classList.remove("hidden")
-                  }
-                  onMouseLeave={(e) =>
-                    e.currentTarget.querySelector("p").classList.add("hidden")
-                  }
-                >
-                  <span className="font-semibold text-xl flex justify-center items-center">
-                    DESCRIÇÃO DO PRODUTO
-                  </span>
-                  <p className="text-lg hidden">{products.description}</p>
-                </div>
-            </div>
-          );
-        })}
-    </main>
+    <div className="h-screen w-screen bg-gradient-to-t from-green-100 via-green-400 to-green-100">
+      <nav className="flex flex-row p-4">
+        <a href="/">
+          <Image src="/logo1.svg" alt="logo" width={120} height={120} />
+        </a>
+      </nav>
+      <main className="flex justify-center items-center flex-col">
+          <h1 className="text-8xl font-bold text-green-900 animate-fade-up animate-duration-[2000ms]">Compras e vendas <br/>direto pelo Whatsapp</h1>
+          <p className="text-6xl font-bold text-green-700 animate-fade-up animate-duration-[2000ms] animate-delay-1000 animate-ease-linear">Tudo com muita rapidez e segurança!</p>
+          <div className="mt-32 px-20 py-7 w-auto h-auto flex flex-col gap-5 items-center justify-center rounded-lg bg-green-200 bg-opacity-55 border-2 border-green-700 animate-fade-up animate-duration-[2000ms] animate-delay-1000 animate-ease-linear">
+            <h2 className="font-bold text-3xl">Inscreva-se</h2>
+           <div className="flex flex-row gap-4">
+            <input placeholder="Digite seu número" className="w-full p-2 rounded-lg border-2 border-green-700 " />
+            <button className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-lg ">Entrar</button>
+           </div>
+          </div>
+      </main>
     </div>
   );
 }
